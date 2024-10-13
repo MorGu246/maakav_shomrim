93a9e2a9a502910ace097f1f001337fab45afdce
MorGu246 <morgueta36@gmail.com>
2024-10-13 19:26:05 +0300

    maakav_shomrim java script
    תצוגה אחורית (Insomnia)
    לקריאה, כתיבה, עריכה ומחיקה

---
+++ index_final.js
@@ -0,0 +1,46 @@
+const port = 4329;
+
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
+const path=require('path');
+app.use(express.static(path.join(__dirname,"")));
+
+let points=[];
+
+app.get('/pointslist',(req,res) => {
+res.status(200).json(points);
+});
+
+app.post('/Addpoint',(req,res) => {
+    let ThisPoint={};
+    ThisPoint.name=req.body.name;
+    ThisPoint.num=req.body.num;
+    ThisPoint.time=req.body.time;
+    points.push(ThisPoint);
+    res.status(200).json("נקודה נוספה");
+});
+
+app.patch('/Editpoint/:idx',(req,res) => {
+    let ThisPoint={};
+    let idx=req.params.idx;
+    ThisPoint.name=req.body.name;
+    ThisPoint.num=req.body.num;
+    ThisPoint.time=req.body.time;
+    points[idx]=ThisPoint;
+    res.status(200).json("נקודה עודכנה");
+});
+
+app.delete('/Deletepoint/:myid',(req,res) => {
+    let idx=req.params.myid;
+    points.splice(idx,1)
+    res.status(200).json(points);
+});
+
+app.listen(port, () => {
+    console.log(`Now listening on port http://localhost:${port}`);
+});
