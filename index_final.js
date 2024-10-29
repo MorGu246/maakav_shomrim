2106ffd2e3b5dc0a96e68ac896d4c0f61a417502
MorGu246 <morgueta36@gmail.com>
2024-10-30 00:11:56 +0200

    maakav_shomrim java script part 6
    עכשיו הזמן של הנקודה של השמירה נשמר אוטומטית בשמירה/עדכון של נקודת ביקור שומרים
    הזמן מעודכן לפי זמן ישראל

--- index_final.js
+++ index_final.js
@@ -10,6 +10,35 @@ app.use(bodyParser.urlencoded({extended: false}));
 const path=require('path');
 app.use(express.static(path.join(__dirname,"")));
 
+let points=[];
+
+app.get('/point',(req,res) => {
+res.status(200).json(points);
+});
+
+app.post('/point',(req,res) => {
+    let ThisPoint={};
+    ThisPoint.name=req.body.name;
+    ThisPoint.num=req.body.num;
+    points.push(ThisPoint);
+    res.status(200).json("נקודה נוספה");
+});
+
+app.put('/point',(req,res) => {
+    let ThisPoint={};
+    let idx=req.body.idx;
+    ThisPoint.name=req.body.name;
+    ThisPoint.num=req.body.num;
+    points[idx]=ThisPoint;
+    res.status(200).json("נקודה עודכנה");
+});
+
+app.delete('/point',(req,res) => {
+    let idx=req.body.myid;
+    points.splice(idx,1)
+    res.status(200).json(points);
+});
+
 let pointsVisited=[];
 
 app.get('/pointVisited',(req,res) => {
@@ -43,39 +72,9 @@ app.delete('/pointVisited',(req,res) => {
     res.status(200).json(pointsVisited);
 });
 
-let points=[];
-
-app.get('/point',(req,res) => {
-res.status(200).json(points);
-});
-
-app.post('/point',(req,res) => {
-    let ThisPoint={};
-    ThisPoint.name=req.body.name;
-    ThisPoint.num=req.body.num;
-    points.push(ThisPoint);
-    res.status(200).json("נקודה נוספה");
-});
-
-app.put('/point',(req,res) => {
-    let ThisPoint={};
-    let idx=req.body.idx;
-    ThisPoint.name=req.body.name;
-    ThisPoint.num=req.body.num;
-    points[idx]=ThisPoint;
-    res.status(200).json("נקודה עודכנה");
-});
-
-app.delete('/point',(req,res) => {
-    let idx=req.body.myid;
-    points.splice(idx,1)
-    res.status(200).json(points);
-});
-
-
 app.get('/admin',(req,res) => {
-res.status(200).sendFile(path.join(__dirname,"/nodeJS_final.html"));
-});
+    res.status(200).sendFile(path.join(__dirname,"/nodeJS_final.html"));
+    });
 
 app.get('/',(req,res) => {
 res.status(200).sendFile(path.join(__dirname,"/nodeJS_final.html"));
